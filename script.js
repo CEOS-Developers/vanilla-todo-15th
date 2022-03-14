40bc329cbdc73f6cecea91825d1465895a1309fc
juhyojeong <juhyojeong58@gmail.com>
2022-03-14 11:39:06 +0900

    feat: add deleteItem function

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ const addTodoItem = () => {
   const todoId = todoList.length + 1;
   const newTodo = document.createElement("li");
   const newTodoText = document.createElement("span");
+  const todoDel = document.createElement("img");
+
   newTodoText.innerText = todoContent;
+  newTodoText.style.cursor = "pointer";
+  newTodoText.style.fontSize = "15px";
+
+  todoDel.setAttribute("src", "img/bin.png");
+  todoDel.style.width = "14px";
+  todoDel.style.paddingLeft = "5px";
+  todoDel.style.cursor = "pointer";
+  todoDel.addEventListener("click", deleteTodoItem);
+
   newTodo.setAttribute("id", todoId);
+  newTodo.style.marginBottom = "10px";
   newTodo.appendChild(newTodoText);
+  newTodo.appendChild(todoDel);
 
   const todoObj = {
     id: todoId,
@@ -16,4 +29,12 @@ const addTodoItem = () => {
   todoList.push(todoObj);
   document.querySelector(".todo-list").appendChild(newTodo);
   document.querySelector(".todo-input").value = "";
+};
+const deleteTodoItem = (e) => {
+  const target = e.target.parentNode;
+  document.querySelector(".todo-list").removeChild(target);
+  const newTodoList = todoList.filter(
+    (item) => item.id !== parseInt(target.id),
+  );
+  todoList = newTodoList;
 };
