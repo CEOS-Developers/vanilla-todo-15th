f64d9ba681829cf327de78d6fd764c117a8a3773
Minji Oh <ohminji@ewhain.net>
2022-03-16 19:13:10 +0900

    Determine whether a to do is done or not

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const savedList = document.getElementById('savedList');
+const doneList = document.getElementById('doneList');
 
 form.addEventListener('submit', (e) => {
   const li = document.createElement('li');
   li.innerText = document.getElementById('input').value;
-  savedList.appendChild(li);
   li.id = 'li';
+  li.isDone = false;
 
   const deleteButton = document.createElement('button');
   deleteButton.innerText = 'x';
   deleteButton.id = 'deleteButton';
-  li.appendChild(deleteButton);
+  li.append(deleteButton);
 
-  deleteButton.addEventListener('click', function () {
-    savedList.removeChild(li);
+  li.addEventListener('click', () => {
+    li.isDone ? (li.isDone = false) : (li.isDone = true);
+    li.isDone ? doneList.append(li) : savedList.append(li);
   });
+
+  li.isDone ? doneList.append(li) : savedList.append(li);
 
   e.preventDefault();
 });
