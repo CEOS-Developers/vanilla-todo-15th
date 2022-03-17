9fa867c364a51fd86a3dae37d384c8a35a42de78
전시원 <>
2022-03-17 16:44:17 +0900

    Add input list

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 // DOM 조작(검색)
-
-const input = document.querySelector('#input-form input');
-const plus = document.querySelector('#plus');
-const yetList = document.querySelector('#yet-list');
-const doneList = document.querySelector('#done-list');
-const todoNum = document.querySelector('#todoNum');
-const doneNum = document.querySelector('#doneNum');
+const inputField = document.querySelector('#todo-input');
+const addTodoBtn = document.querySelector('#add-todo-btn');
+const todoListTitle = document.querySelector('#todo-list-title');
+const doneListTitle = document.querySelector('#done-list-title');
+const todoList = document.querySelector('.todo-list');
+const doneList = document.querySelector('.done-list');
 
 // 텍스트는 변수로 설정 => 오타 방지 및 수정 용이
 const todoItemsText = 'todoItems';
 
 // for localStorage
-let todoItems = [];
+let items = [];
 
-const setItems = (e) => {
-  input.value = '';
-  const obj = {
-    contents: input.value,
-    id: Date.now(),
+//
+addTodoBtn.addEventListener('click', (e) => {
+  const val = inputField.value;
+  const todoObj = {
+    id: null,
+    contents: val,
     isDone: false,
   };
-  todoItems.push(obj);
+  inputField.value = '';
+  items.push(todoObj);
+  console.log(items);
   e.preventDefault();
-};
-plus.addEventListener('click', setItems);
-
-const render = () => {};
+});
