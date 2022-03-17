6d7e99e518f513395b53afc76e69b53e592b826f
Minji Oh <ohminji@ewhain.net>
2022-03-17 13:05:51 +0900

    Count number of to dos for each list

--- script.js
+++ script.js
@@ -4,20 +4,36 @@ const doneList = document.getElementById('doneList');
 form.addEventListener('submit', (e) => {
   const li = document.createElement('li');
   li.innerText = document.getElementById('input').value;
-  li.id = 'li';
+  li.id = li.innerText;
   li.isDone = false;
+  li.isDone ? doneList.append(li) : savedList.append(li);
 
   const deleteButton = document.createElement('button');
   deleteButton.innerText = 'x';
   deleteButton.id = 'deleteButton';
   li.append(deleteButton);
 
+  const countSavedNum = () => {
+    let savedNum = savedList.getElementsByTagName('li').length;
+  };
+
+  const countDoneNum = () => {
+    let doneNum = doneList.getElementsByTagName('li').length;
+  };
+
   li.addEventListener('click', () => {
     li.isDone ? (li.isDone = false) : (li.isDone = true);
     li.isDone ? doneList.append(li) : savedList.append(li);
+    countSavedNum();
+    countDoneNum();
   });
 
-  li.isDone ? doneList.append(li) : savedList.append(li);
+  deleteButton.addEventListener('click', function (event) {
+    const targetToDo = event.currentTarget.parentNode;
+    console.log(targetToDo.parentNode);
+    targetToDo.parentNode.remove(targetToDo);
+    targetToDo.remove();
+  });
 
   e.preventDefault();
 });
