e4932532101a8c1f950a976548c9e177c14087d9
YoommY <seeun0792@gmail.com>
2022-03-17 13:32:39 +0900

    배경화면 랜덤으로 바뀜

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-
+const colors = [
+    "#0fbcf9",
+    "#00d8d6",
+    "#05c46b",
+    "#ffc048",
+    "#ffdd59"
+  
+    ];
+  
+  
+    
+  
+    let firstColor = colors[Math.floor(Math.random() * colors.length)];
+    let secondColor = colors[Math.floor(Math.random() * colors.length)];
+    
+    document.body.style.background = `linear-gradient(${firstColor}, ${secondColor})`;
    
