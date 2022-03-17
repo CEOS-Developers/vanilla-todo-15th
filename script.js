9244491602d36086240489273f00ff2fe5270239
전시원 <>
2022-03-17 11:06:30 +0900

    Change structure
    Change eventListen form => input button click

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // DOM 조작(검색)
-const form = document.querySelector('#input-form');
+
 const input = document.querySelector('#input-form input');
+const plus = document.querySelector('#plus');
 const yetList = document.querySelector('#yet-list');
 const doneList = document.querySelector('#done-list');
 const todoNum = document.querySelector('#todoNum');
@@ -11,63 +12,17 @@ const todoItemsText = 'todoItems';
 
 // for localStorage
 let todoItems = [];
-let id = 0;
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const val = input.value;
+const setItems = (e) => {
   input.value = '';
   const obj = {
-    contents: val,
+    contents: input.value,
     id: Date.now(),
     isDone: false,
   };
   todoItems.push(obj);
-  render(obj);
-  setLocal(todoItems);
-});
-
-const render = (obj) => {
-  // rendering in todoList
-  const li = document.createElement('li');
-  const btn = document.createElement('button');
-  const delBtn = document.createElement('button');
-  btn.innerText = obj.contents;
-  delBtn.innerText = 'X';
-  li.id = obj.id;
-  li.status = obj.isDone;
-  li.append(btn);
-  li.append(delBtn);
-  yetList.append(li);
-
-  // add eventListener
-  delBtn.addEventListener('click', (e) => {
-    const target = e.target.parentElement;
-    const id = target.id;
-    target.remove();
-
-    const temp = JSON.parse(localStorage.getItem(todoItemsText));
-    const stillTodo = temp.filter((i) => i.id != id); // 눌러지지 않은 것들
-    localStorage.setItem(todoItemsText, JSON.stringify(stillTodo));
-  });
-  btn.addEventListener('click', (e) => {
-    e.target.parentElement.remove();
-    const target = e.target.parentElement;
-    const id = target.id;
-    target.status = true;
-
-    const li = document.createElement('li');
-    const btn = document.createElement('button');
-    btn.innerText = target.innerText.slice(0, -1);
-    li.append(btn);
-    doneList.append(li);
-
-    btn.addEventListener('click', (e) => {
-      const target = e.target.parentElement;
-    });
-  });
+  e.preventDefault();
 };
+plus.addEventListener('click', setItems);
 
-const setLocal = (arr) => {
-  localStorage.setItem(todoItemsText, JSON.stringify(arr));
-};
+const render = () => {};
