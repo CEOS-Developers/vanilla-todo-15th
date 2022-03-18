b122e7c95bde6b84507b77f6399e2d5f84d2c39b
Minji Oh <ohminji@ewhain.net>
2022-03-18 14:36:14 +0900

    Refactor JavaScript code

--- script.js
+++ script.js
@@ -5,30 +5,36 @@ savedListTitle.innerHTML = '해야할 일';
 doneListTitle.innerHTML = '완료한 일';
 
 form.addEventListener('submit', (e) => {
+  //할 일 개수 세기
   const countSavedNum = () => {
     let savedNum = savedList.getElementsByTagName('li').length;
-    savedListTitle.innerHTML = `해야할 일(${savedNum + 1})`;
+    savedListTitle.innerHTML = `해야할 일(${savedNum})`;
   };
-  countSavedNum();
 
   const countDoneNum = () => {
     let doneNum = doneList.getElementsByTagName('li').length;
     doneListTitle.innerHTML = `완료한 일(${doneNum})`;
   };
-  countDoneNum();
 
+  //할 일 구현
   const li = document.createElement('li');
   li.innerText = document.getElementById('input').value;
   li.id = li.innerText;
   li.isDone = false;
   li.isDone ? doneList.append(li) : savedList.append(li);
+  countSavedNum();
 
+  //삭제 버튼
   const deleteButton = document.createElement('button');
   deleteButton.innerText = 'x';
   deleteButton.id = 'deleteButton';
   li.append(deleteButton);
 
-  li.draggable = true;
+  deleteButton.addEventListener('click', function (event) {
+    setTimeout(() => li.remove(), 0);
+  });
+
+  //할 일이 완료 되었는지 확인
   li.addEventListener('click', () => {
     li.isDone ? (li.isDone = false) : (li.isDone = true);
     li.isDone ? doneList.append(li) : savedList.append(li);
@@ -36,22 +42,17 @@ form.addEventListener('submit', (e) => {
     countDoneNum();
   });
 
+  //할 일에 드래그 속성 추가
+  li.draggable = true;
   li.addEventListener('dragstart', function (event) {});
-
   li.addEventListener('drag', function (event) {});
-
   li.addEventListener('dragover', function (event) {
     event.preventDefault();
   });
-
   li.addEventListener('drop', function (event) {
     event.preventDefault();
     console.log(event.target);
     event.target.parentNode.append(li);
-  });
-
-  deleteButton.addEventListener('click', function (event) {
-    setTimeout(() => li.remove(), 0);
   });
 
   e.preventDefault();
