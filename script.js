1591ce55a6ec2d3e39158271053f771b565ccdbd
poodlepoodle <chammal97@naver.com>
2022-03-19 06:15:23 +0900

    Prettier 적용

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ let indPre = 0;
 let indDone = 0;
 
 const createToDo = (event) => {
-    event.preventDefault();
-  
-    const toDoObj = {
-        done: false,
-      text: toDoInput.value,
-      id: indPre + 1,
-    };
-  
-    drawToDo(toDoObj);
-    toDoInput.value = "";
-    indPre += 1;
+  event.preventDefault();
 
-    saveListCache();
+  const toDoObj = {
+    done: false,
+    text: toDoInput.value,
+    id: indPre + 1,
+  };
+
+  drawToDo(toDoObj);
+  toDoInput.value = "";
+  indPre += 1;
+
+  saveListCache();
 };
 
 const drawToDo = (toDoObj) => {
@@ -31,7 +31,7 @@ const drawToDo = (toDoObj) => {
   span.addEventListener("click", toggleToDo);
   span.innerHTML = toDoObj.text;
   li.appendChild(span);
-  
+
   const delButton = document.createElement("img");
   delButton.src = "img/ios_trash.png";
   delButton.addEventListener("click", deleteToDo);
@@ -40,116 +40,123 @@ const drawToDo = (toDoObj) => {
   li.id = toDoObj.id;
 
   if (toDoObj.done === false) {
-      toDoListPre.appendChild(li);
-      document.getElementById("pre").innerHTML = Number(document.getElementById("pre").innerHTML) + 1;
-    }
-    else {
-        toDoListDone.appendChild(li);
-        document.getElementById("done").innerHTML = Number(document.getElementById("pre").innerHTML) + 1;
-    }
+    toDoListPre.appendChild(li);
+    document.getElementById("pre").innerHTML =
+      Number(document.getElementById("pre").innerHTML) + 1;
+  } else {
+    toDoListDone.appendChild(li);
+    document.getElementById("done").innerHTML =
+      Number(document.getElementById("pre").innerHTML) + 1;
+  }
 };
 
-
 const deleteToDo = (event) => {
-    const { target } = event;
-    const li = target.parentNode;
-    const done = (li.parentNode.className == "todo-list-done");
-    
-    if (!done) {
-        toDoListPre.removeChild(li);
-        document.getElementById("pre").innerHTML = Number(document.getElementById("pre").innerHTML) - 1;
-    }
-    else {
-        toDoListDone.removeChild(li);
-        document.getElementById("done").innerHTML = Number(document.getElementById("pre").innerHTML) - 1;
-    }
+  const { target } = event;
+  const li = target.parentNode;
+  const done = li.parentNode.className == "todo-list-done";
 
-    saveListCache();
-  };
+  if (!done) {
+    toDoListPre.removeChild(li);
+    document.getElementById("pre").innerHTML =
+      Number(document.getElementById("pre").innerHTML) - 1;
+  } else {
+    toDoListDone.removeChild(li);
+    document.getElementById("done").innerHTML =
+      Number(document.getElementById("pre").innerHTML) - 1;
+  }
 
+  saveListCache();
+};
 
 const toggleToDo = (event) => {
-    const { target } = event;
-    const li = target.parentNode;
-    const done = (li.parentNode.className == "todo-list-done");
+  const { target } = event;
+  const li = target.parentNode;
+  const done = li.parentNode.className == "todo-list-done";
 
-    if (!done) {
-        toDoListPre.removeChild(li);
-        toDoListDone.appendChild(li);
-        document.getElementById("pre").innerHTML = Number(document.getElementById("pre").innerHTML) - 1;
-        document.getElementById("done").innerHTML = Number(document.getElementById("done").innerHTML) + 1;
-        indDone += 1;
-    }
-    else {
-        toDoListDone.removeChild(li);
-        toDoListPre.appendChild(li);
-        document.getElementById("pre").innerHTML = Number(document.getElementById("pre").innerHTML) + 1;
-        document.getElementById("done").innerHTML = Number(document.getElementById("done").innerHTML) - 1;
-        indPre += 1;
-    }
+  if (!done) {
+    toDoListPre.removeChild(li);
+    toDoListDone.appendChild(li);
+    document.getElementById("pre").innerHTML =
+      Number(document.getElementById("pre").innerHTML) - 1;
+    document.getElementById("done").innerHTML =
+      Number(document.getElementById("done").innerHTML) + 1;
+    indDone += 1;
+  } else {
+    toDoListDone.removeChild(li);
+    toDoListPre.appendChild(li);
+    document.getElementById("pre").innerHTML =
+      Number(document.getElementById("pre").innerHTML) + 1;
+    document.getElementById("done").innerHTML =
+      Number(document.getElementById("done").innerHTML) - 1;
+    indPre += 1;
+  }
 
-    saveListCache();
+  saveListCache();
 };
 
 const saveListCache = () => {
-    saveList = [];
-    
-    saveList.push(Number(document.querySelectorAll(".todo-list-pre > li > span").length));
-    for (let toDoObj of document.querySelectorAll(".todo-list-pre > li > span")) {
-        saveList.push(toDoObj['innerHTML'])
-    }
-    
-    for (let toDoObj of document.querySelectorAll(".todo-list-done > li > span")) {
-        saveList.push(toDoObj['innerHTML'])
-    }
+  saveList = [];
 
-    localStorage.setItem("toDoListCache", JSON.stringify(saveList));
-  };
-  
+  saveList.push(
+    Number(document.querySelectorAll(".todo-list-pre > li > span").length)
+  );
+  for (let toDoObj of document.querySelectorAll(".todo-list-pre > li > span")) {
+    saveList.push(toDoObj["innerHTML"]);
+  }
 
-const loadListCache = () => {
-    const loadedToDoList = localStorage.getItem("toDoListCache");
+  for (let toDoObj of document.querySelectorAll(
+    ".todo-list-done > li > span"
+  )) {
+    saveList.push(toDoObj["innerHTML"]);
+  }
 
-    console.log(loadedToDoList);
-
-    const lenPre = Number(loadedToDoList[0]);
-
-    for (let index = 0; index < lenPre; index++) {
-        drawToDo({
-            done: false,
-          text: loadedToDoList[index],
-          id: indPre + 1,
-        });
-        indPre += 1;
-    }
-    for (let index = lenPre; index < loadedToDoList.length; index++) {
-        drawToDo({
-            done: true,
-            text: loadedToDoList[index],
-          id: indDone + 1,
-        });
-        indDone += 1;
-    }
-  };
-
-  const setClock = () => {
-    var today = new Date()
-var hh = addZero(today.getHours())
-var mm = addZero(today.getMinutes())
-var YY = today.getFullYear()
-var MM = today.getMonth()+1
-var DD = today.getDate()
-
-    document.getElementById("time").innerHTML = hh + ":" + mm;
-    document.getElementById("date").innerHTML = YY + "년 " + MM + "월 " + DD + "일" + "  " + hh + ":" + mm;
+  localStorage.setItem("toDoListCache", JSON.stringify(saveList));
 };
 
-const addZero = (num) => (num < 10 ? '0' + num : '' + num);
+const loadListCache = () => {
+  const loadedToDoList = localStorage.getItem("toDoListCache");
+
+  console.log(loadedToDoList);
+
+  const lenPre = Number(loadedToDoList[0]);
+
+  for (let index = 0; index < lenPre; index++) {
+    drawToDo({
+      done: false,
+      text: loadedToDoList[index],
+      id: indPre + 1,
+    });
+    indPre += 1;
+  }
+  for (let index = lenPre; index < loadedToDoList.length; index++) {
+    drawToDo({
+      done: true,
+      text: loadedToDoList[index],
+      id: indDone + 1,
+    });
+    indDone += 1;
+  }
+};
+
+const setClock = () => {
+  var today = new Date();
+  var hh = addZero(today.getHours());
+  var mm = addZero(today.getMinutes());
+  var YY = today.getFullYear();
+  var MM = today.getMonth() + 1;
+  var DD = today.getDate();
+
+  document.getElementById("time").innerHTML = hh + ":" + mm;
+  document.getElementById("date").innerHTML =
+    YY + "년 " + MM + "월 " + DD + "일" + "  " + hh + ":" + mm;
+};
+
+const addZero = (num) => (num < 10 ? "0" + num : "" + num);
 
 const init = () => {
-    loadListCache();
-    setClock();
-    setInterval(setClock, 1000);
+  loadListCache();
+  setClock();
+  setInterval(setClock, 1000);
 
   toDoForm.addEventListener("submit", createToDo);
   toDoInputBtn.addEventListener("click", createToDo);
